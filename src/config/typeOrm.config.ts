6478fc27360cee14c20770c10e4e24b5fc2b21b7
Deni setiawan <denistwn67@gmail.com>
2023-07-31 21:40:04 +0700

    update

--- src/config/typeOrm.config.ts
+++ src/config/typeOrm.config.ts
@@ -1,12 +1,27 @@
+// import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+// export const typeOrmConfig: TypeOrmModuleOptions = {
+//     type: 'mysql',
+//     host: 'localhost',
+//     port: 3306,
+//     username: 'root',
+//     password: 'root',
+//     database: 'db_kinicoffee',
+//     // entities: [Product],
+//     entities: [__dirname + '/../**/*.entity.{ts,js}'],
+//     autoLoadEntities: true,
+//     synchronize: false
+// }
+
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: 'localhost',
+  host: process.env.DATABASE_HOST,
   port: 3306,
-  username: 'kopicoff_strapi',
-  password: 'kini_Coffee2023',
-  database: 'kopicoff_db',
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   // entities: [Product],
   entities: [__dirname + '/../**/*.entity.{ts,js}'],
   autoLoadEntities: true,
