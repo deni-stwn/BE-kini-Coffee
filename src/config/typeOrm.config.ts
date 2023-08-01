4c018f3082db6ced8d7f29c8951462c94ef68ae7
Deni setiawan <denistwn67@gmail.com>
2023-08-01 10:45:08 +0700

    update

--- src/config/typeOrm.config.ts
+++ src/config/typeOrm.config.ts
@@ -13,12 +13,31 @@
 //     synchronize: false
 // }
 
+// import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+// export const typeOrmConfig: TypeOrmModuleOptions = {
+//   type: 'mysql',
+//   host: 'localhost',
+//   port: 3306,
+//   username: 'kopicoff_strapi',
+//   password: 'kini_Coffee2023',
+//   database: 'kopicoff_db',
+//   // entities: [Product],
+//   entities: [__dirname + '/../**/*.entity.{ts,js}'],
+//   autoLoadEntities: true,
+//   synchronize: false,
+// };
+
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+
+// Panggil config() untuk mengonfigurasi variabel lingkungan dari file .env
+config();
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mysql',
   host: process.env.DATABASE_HOST,
-  port: 3306,
+  port: parseInt(process.env.DATABASE_PORT, 10),
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
@@ -27,3 +46,4 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   autoLoadEntities: true,
   synchronize: false,
 };
+
